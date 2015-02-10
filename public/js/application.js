b7213925715a3fed757b3e0ca1fcabd63d67e39b
Iqlaas <kheryee@live.com>
2015-02-10 15:42:37 +0800

    Finished Recent Tweet 2

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,21 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $.ajax({
+    url: location.pathname,
+    method: 'GET',
+    dataType: 'html',
+    beforeSend: function(){
+      $('#loading').show();
+      console.log('beforeSend');
+    },
+    success: function(){
+      $('.tweets').show();
+      $('#loading').hide();
+    }
+
+  });
+  // event.preventDefault();
+
 });
